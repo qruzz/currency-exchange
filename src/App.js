c1f90cefa8ae680f72ac00a70d78b5f8cc87ab97
Michael Nissen <m9ssen@gmail.com>
2018-12-26 10:36:51 +0100

    Button active switch state implemented

--- src/App.js
+++ src/App.js
@@ -1,10 +1,68 @@
 import React from 'react';
+import Button from './components/Button';
+import CurrencyInput from './components/CurrencyInput';
 
+/**
+ * 
+ */
 class App extends React.PureComponent {
+	/**
+	 * 
+	 * @param {*} props 
+	 */
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			exchangeFromCurrency: 'GBP',
+			exchangeFromAmount: '',
+			exchangeToCurrency: 'EUR',
+			exchangeToAmount: '',
+		};
+	}
+
+	handleOnChange = (type, event) => {
+		const value = event.target.value;
+		this.setState({ [type]: value });
+	}
+
+	canExcange = () => {
+		const { exchangeFromAmount, exchangeToAmount } = this.state;
+
+		if (exchangeFromAmount === '' || exchangeToAmount === '') {
+			return (false);
+		}
+
+		return (true);
+	}
+
+	/**
+	 * 
+	 */
 	render() {
+		console.log(this.state);
+		const {
+			exchangeFromAmount,
+			exchangeFromCurrency,
+			exchangeToAmount,
+			exchangeToCurrency,
+		} = this.state;
 		return (
 			<div className="App">
-
+				<CurrencyInput
+					handleOnChange={this.handleOnChange}
+					value={exchangeFromAmount}
+					currency={exchangeFromCurrency}
+				/>
+				<CurrencyInput
+					handleOnChange={this.handleOnChange}
+					value={exchangeToAmount}
+					currency={exchangeToCurrency}
+				/>
+				<Button
+					title={'Exchange'}
+					active={this.canExcange()}
+				/>
 			</div>
 		);
 	}
